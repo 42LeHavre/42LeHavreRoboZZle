a2fcdcd0f0a24a9e2f5e0d2c88e4f5a82e98cc61
DinoMalin <jcario@student.42lehavre.fr>
2024-04-21 16:15:41 +0200

    ~ | Fixed orthograph

--- src/src/game.js
+++ src/src/game.js
@@ -3,17 +3,17 @@ let stopValue = false;
 let data;
 
 /*
-	la classe Data est destinée à être global pour ce fichier-là mais aussi pour l'affichage à la map.
-	un tableau nommait map représentant la map (ligne en x et collone en y);
-	un entier nommait x représentant la position du payeur en x;
-	un entier nommait y représentant la position du payeur en y;
-	une chaine de caractère nommer dir représentant la position du payeur comme ceci;
+	la classe Data est destinée à être globale pour ce fichier-là mais aussi pour l'affichage à la map.
+	un tableau nomme map représentant la map (ligne en x et colonne en y);
+	un entier nomme x représentant la position du player en x;
+	un entier nomme y représentant la position du player en y;
+	une chaine de caractèred nommer dir représentant la position du player comme ceci;
 	"up" vers le haut
 	"down" vers le bas
 	"left" vers la gauche
 	"right" vers la doite
 
-	et un entier nommait _nbCollectible représentant le nombre de collectible restant
+	et un entier nomme _nbCollectible représentant le nombre de collectibles restant
 */
 
 class Data{
@@ -92,8 +92,8 @@ function stopGame()
 	stopValue = true;
 }
 
-//l'affichage a besoin d'une classe Game pour pouvoir appeler la fonction constructGame mais aussi pour pouvoir afficher la map par defaut et les instruction autoriser,
-//pour cela il doit appeler la fonction createInstance avec en paremetre le nom du niveau souhaiter
+//l'affichage a besoin d'une classe Game pour pouvoir appeler la fonction constructGame mais aussi pour pouvoir afficher la map par defaut et les instruction autorisees,
+//pour cela il doit appeler la fonction createInstance avec en parametre le nom du niveau souhaite
 
 
 
@@ -300,16 +300,16 @@ async function startFunction(gameInstance, listToDo)
 
 /*
 
-pour pouvoir démarrer une gamme vous avez besoin d'une instance de la classe game; 
-pour cela vous avez 2 possibilités, le premier : crée votre class vous-même, ce qui est un peu chiant est sujet à erreur.
-c'est pour cela que ma grande personne à l'extrême générosité a pris la sage décision de créer une fonction qui le fait pour vous et vous retourne une instance de cette classe.
+pour pouvoir démarrer une game vous avez besoin d'une instance de la classe game; 
+pour cela vous avez 2 possibilités, le premier : créer votre classe vous-même, ce qui est un peu chiant et sujet à erreur.
+c'est pour cela que ma grande personne a l'extrême générosité a pris la sage décision de créer une fonction qui le fait pour vous et vous retourne une instance de cette classe.
 pour cela rien de plus simple, appeler la fonction createInstance("nameOfLvl") qui prend en paramètre le nom du level en question,
-tant que ce nom-là est bien un fichier .json existant dans le dossier public/maps/ et qu'il est bien formaté (même pas besoin de mettre le path ou le .json à la fin car de toute évidence je suis trop généreux et vous facilite le travaille).
+tant que ce nom-là est bien un fichier .json existant dans le dossier public/maps/ et qu'il est bien formaté (même pas besoin de mettre le path ou le .json à la fin car de toute évidence je suis trop généreux et vous facilite le travail).
 
-cette fonction ne se contente pas seulement de vous retourner une class Game, elle instancie aussi une class Data déclarée en variable global nommer data qui vous permet d'avoir accès à la map, la position de votre joueur et le nombre de collectible restant.
-elle est declarer comme tel : 
+cette fonction ne se contente pas seulement de vous retourner une classe Game, elle instancie aussi une classe Data déclarée en variable globale nommee data qui vous permet d'avoir accès à la map, la position de votre joueur et le nombre de collectible restant.
+elle est declaree comme telle : 
 
-class Data{
+class Data {
 	constructor(_map, _x, _y, _dir, _nbCollectible) {
 		this.map = _map;
 		this.x = _x;
@@ -319,35 +319,35 @@ class Data{
 	}
 }
 
-Tout changement effectué à la map(probablement par la fonction au prochain paragraphe), a la position du payeur ou encore au nombre de collectible sera effectué dans cette variable globale.
+Tout changement effectué à la map(probablement par la fonction au prochain paragraphe), a la position du player ou encore au nombre de collectibles sera effectué dans cette variable globale.
 
 
-bienvenue dans la fonction constructGame(), cette fonction permet de start la gamme mais aussi de la jouer entièrement, tout ce dont tu as besoin se situe dans la Class data.
+bienvenue dans la fonction constructGame(), cette fonction permet de start la game mais aussi de la jouer entièrement, tout ce dont tu as besoin se situe dans la classe data.
 et si jamais tu souhaites changer la vitesse entre chaque tour il existe une variable globale nommée deltaTime qui est en ms, tu peux la modifier quand tu veux.
 
-pour démarrer une game appelée la fonction constructGame(), elle prend en paramètre une classe Game (qui peut être construite avec la fonction createInstance("nameOfLvl"), voir plus haut)
-SI JAMAIS TU VEUX LA CONSTRUIRE A LA MAIN VOICI COMMENT ELLE CE COMPORTE : 
-elle ce construit avec 2 argument, 
-Le premier est une chaine de caractère qui représente le nom du lvl (mais aussi le nom du fichier json en question);
-Le deuxième est un object nomer list_instruction(f1,f2,fx...) qui Contient un tableau avec en premier colonne une chaine de caractère et en deuxième une class d'instruction prenant le mouvement effectué et la couleur de la case
-formater comme ceci :
+pour démarrer une game appelez la fonction constructGame(), elle prend en paramètre une classe Game (qui peut être construite avec la fonction createInstance("nameOfLvl"), voir plus haut)
+SI JAMAIS TU VEUX LA CONSTRUIRE A LA MAIN VOICI COMMENT ELLE SE COMPORTE : 
+elle Se construit avec 2 arguments, 
+Le premier est une chaine de caractères qui représente le nom du lvl (mais aussi le nom du fichier json en question);
+Le deuxième est un objet nomme list_instruction(f1,f2,fx...) qui contient un tableau avec en premiere colonne une chaine de caractères et en deuxième une classe instruction prenant le mouvement effectué et la couleur de la case
+formate comme ceci :
 
 type de mouvement : 
 
-"F1", "F2", "Fx..." (potentielement un nombre infinie de f)
+"F1", "F2", "Fx..." (potentiellement un nombre infini de f)
 "forward" (pour avancer)
-"left" (pour aller a gauche (ouest pour julian))
-"right" (pour aller a droite (est pour julian))
-null (si la personne n'as pas donner d'instruction a cette case)
+"left" (pour aller a gauche (ouest pour julian)) oui - julian
+"right" (pour aller a droite (est pour julian)) oui - julian
+null (si la personne n'a pas donne d'instruction a cette case)
 
 type de couleur :
 
 "blue" (pour la couleur bleu)
 "red" (pour la couleur rouge)
 "green" (pour la couleur verte)
-null (si la case n'as pas de couleur)
+null (si la case n'a pas de couleur)
 
-voici un exemble de comment l'on peut start une game : 
+voici un exemple de comment on peut start une game : 
 
 
 let level = "level_1";
@@ -380,7 +380,7 @@ let gameInstance = new Game(level, list_instruction);
 constructGame(gameInstance);
 
 
-et voic une facon UN PEUT plus simple et sans erreur pour le faire. 
+et voici une facon UN PEU plus simple et sans erreur pour le faire. 
 */
 
 constructGame(await createInstance("level_1"));
